4fd72c75642619aeeec1fd02eaf53aad6384652a
유하람 <dlfnadl97@naver.com>
2024-11-11 14:00:45 +0900

    프로젝트 생성

---
+++ MJHR_Boot/src/main/java/com/mjhr/MjhrBootApplication.java
@@ -0,0 +1,13 @@
+package com.mjhr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MjhrBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MjhrBootApplication.class, args);
+	}
+
+}
---
+++ MJHR_Boot/src/test/java/com/mjhr/MjhrBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mjhr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MjhrBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
