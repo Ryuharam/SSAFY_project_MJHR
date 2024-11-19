bc20ab0855610406b62329be51c37d74dfb6cd67
김민지 <0516min@naver.com>
2024-11-19 20:53:34 +0900

    머지

--- MJHR_Boot/src/main/java/com/mjhr/project/user/dao/UserDao.java
+++ MJHR_Boot/src/main/java/com/mjhr/project/user/dao/UserDao.java
@@ -6,12 +6,12 @@ import java.util.Map;
 import org.apache.ibatis.annotations.Mapper;
 
 import com.mjhr.project.user.dto.User;
+import com.mjhr.project.user.dto.UserFile;
 
 @Mapper
 public interface UserDao {
 	//사용자 전체 목록
 	public List<User> selectAllUsers();
-	
 	//사용자 등록
 	public int insertUser(User user);
 	//사용자 조회
@@ -20,6 +20,8 @@ public interface UserDao {
 	public int deleteUser(int id);	
 	//사용자 닉네임 검색
 	public User selectUserByNic(String nic);
+	//사용자 파일 경로 저장 
+	public int uploadUserFile(UserFile userfile);
 	
 //	//사용자 정보 수정
 //	public boolean updateUser(User user);
