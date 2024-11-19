b8e53464ed5df26d78d620ed036164f55470de0f
유하람 <dlfnadl97@naver.com>
2024-11-19 17:59:37 +0900

    review controller 작성중

--- MJHR_Boot/src/main/java/com/mjhr/project/review/controller/ReviewController.java
+++ MJHR_Boot/src/main/java/com/mjhr/project/review/controller/ReviewController.java
@@ -5,9 +5,11 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -53,24 +55,44 @@ public class ReviewController {
 	@Operation(summary = "사용자 독후감 조회", description = "사용자가 작성한 독후감을 조회합니다")
 	@GetMapping("/myreview/{userNic}")
 	public ResponseEntity<?> getMyReview(@PathVariable("userId") String userId){
-		return null;
+		SearchCondition condition = new SearchCondition();
+		condition.setKey("user_id");
+		condition.setWord(userId);
+		return searchReviewByKey(condition);
 	}
 
 	@Operation(summary = "도서 독후감 조회", description = "도서에 관련된 독후감을 조회합니다")
 	@GetMapping("/book/{isbn}")
 	public ResponseEntity<?> getBookReview(@PathVariable("isbn") String isbn){
-		return null;
+		SearchCondition condition = new SearchCondition();
+		condition.setKey("isbn");
+		condition.setWord(isbn);
+		return searchReviewByKey(condition);
 	}
 	
 	@Operation(summary = "리뷰 검색", description = "검색 조건에 따라 독후감을 검색합니다")
 	@GetMapping("/search")
 	public ResponseEntity<?> searchReviewByKey(@ModelAttribute SearchCondition condition){
-		return null;
+		List<Review> result = service.searchReview(condition);
+		if(result==null || result.size()==0) return new ResponseEntity<>("리뷰를 찾지 못했습니다.", HttpStatus.NOT_FOUND); 
+		return new ResponseEntity<>(result, HttpStatus.OK);
 	}
 	
 	// 리뷰 등록
+	@Operation(summary = "리뷰 등록", description = "리뷰 작성")
+	@PostMapping()
+	public ResponseEntity<?> registReview(@ModelAttribute Review review){
+		if(service.registReview(review)) return new ResponseEntity<>("등록 성공",HttpStatus.OK);
+		return new ResponseEntity<>("등록 실패", HttpStatus.BAD_REQUEST);
+	}
 	
 	// 리뷰 삭제
+	@Operation(summary = "리뷰 삭제", description = "리뷰 삭제")
+	@DeleteMapping("/delete/{reviewId}")
+	public ResponseEntity<?> removeReview(@PathVariable("reviewId") String reviewId){
+		return null;
+		
+	}
 	
 	// 리뷰 수정
 
--- MJHR_Boot/src/main/java/com/mjhr/project/review/service/ReviewService.java
+++ MJHR_Boot/src/main/java/com/mjhr/project/review/service/ReviewService.java
@@ -3,12 +3,17 @@ package com.mjhr.project.review.service;
 import java.util.List;
 
 import com.mjhr.project.review.dto.Review;
+import com.mjhr.project.review.dto.SearchCondition;
 
 public interface ReviewService {
 
 	List<Review> getAllReview();
 
 	Review getReviewByReviewId(String reviewId);
+
+	List<Review> searchReview(SearchCondition condition);
+
+	boolean registReview(Review review);
 	
 
 }
