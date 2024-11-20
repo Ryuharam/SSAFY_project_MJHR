3ab881ef16acf259748c344801c8eaaf01be220e
김민지 <0516min@naver.com>
2024-11-20 18:02:27 +0900

    1120

---
+++ vue-project/src/components/user/UserLogin.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <h3>로그인 화면</h3>
+        <input type="text" placeholder="아이디를 입력해 주세요."/>
+        <input type="password" placeholder="비밀번호를 입력해 주세요."/>
+        <button @click="login">로그인</button>
+    </div>
+</template>
+
+<script setup>
+import {ref} from 'vue';
+
+
+</script>
+
+<style scoped>
+
+</style>
